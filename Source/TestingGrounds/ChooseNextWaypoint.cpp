60950ff1ccf01bf2cad0fc914104b1a52e71807a
Premshay <premshay@gmail.com>
2019-01-15 11:20:55 +0200

    S05L244 PatrollingGuard CPP + patrolling function done

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,12 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" //TODO remove coupling
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	//TODO protect against empty patrol routes
+	
+	//Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
 
-	//UE_LOG(LogTemp, Warning, TEXT("AI in CPP"));
+	//Set next waypoint
+	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackBoardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+	
+	//Cycle Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 	return EBTNodeResult::Succeeded;
 }
 
